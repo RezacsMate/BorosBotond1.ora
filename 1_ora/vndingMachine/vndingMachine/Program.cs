ff36c71066089e7d9ab03f36c29890f9b1f4f98d
RezacsMate <Rezacs.Mate@C19-05>
2026-09-14 14:10:10 +0200

    basic skeleton

---
+++ 1_ora/vndingMachine/vndingMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace vndingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Friend!");
+        }
+    }
+}
